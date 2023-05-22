6b34b45fc71c8499d8709454ee96bf51b830874f
Cesar269 <cesarin-268@hotmail.com>
2023-05-21 21:46:14 -0600

    nav bar material ui

--- ecare-front/src/Pages/Inicio.js
+++ ecare-front/src/Pages/Inicio.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "../Components/Header";
 
-export default function Inicio () {
+export default function Inicio() {
     return (
         <div>
-         <h1>aaaa</h1>
+            <Header></Header>
         </div>
     );
 }
